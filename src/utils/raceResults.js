f18c638fd02cb7e3097c28ab75662a65c95e39a8
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2023-03-04 14:37:39 -0500

    depricated

--- src/utils/raceResults.js
+++ src/utils/raceResults.js
@@ -5,7 +5,8 @@ const cheerio = require("cheerio");
 var resultUrl = "https://www.formula1.com/en/results.html/2023/races/1141/";
 
 export const getRaceResults = async (raceName, raceType) => {
-  const resultUrl = `https://www.formula1.com/en/results.html/2023/races/1141/${raceName}/${raceType}.html`;
+  const resultUrl =
+    "https://www.skysports.com/f1/grandprix/bahrain/results/2023/practice-1"; //`https://www.formula1.com/en/results.html/2023/races/1141/${raceName}/${raceType}.html`;
   axios.get(resultUrl).then((response) => {
     console.log(response);
   });
