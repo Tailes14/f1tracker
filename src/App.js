62362def9944efc64a717c2fddb10ec7c39e835d
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2023-03-04 15:35:36 -0500

    working on table

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   batch,
   setDoc,
   addDoc,
+  getDoc,
   doc,
 } from "firebase/firestore";
 import "./App.css";
@@ -27,6 +28,8 @@ function App() {
   const [selectRaceType, setSelectRaceType] = useState("");
   var raceTypeSelectOptions = [];
   var raceNamesSelectOptions = [];
+  var databaseSessions = [];
+  var databaseData = [];
 
   for (const raceType of raceTypeUrls) {
     raceTypeSelectOptions.push({ value: raceType, label: raceType });
@@ -43,22 +46,6 @@ function App() {
     getRaceResults(selectRaceName, selectRaceType);
   };
 
-  const getRaces = async () => {
-    //const raceRef = collection(db, "2023", "Races", "Bahrain");
-    /*
-    await getDocs(collection(db, "2023")).then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-        getDocs(collection(db, "2023", doc.id)).then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-          });
-        });
-      });
-    });
-    */
-  };
-
   const handleRaceNameChange = (e) => {
     setAddRaceName(e.target.value);
   };
@@ -71,32 +58,60 @@ function App() {
     setSelectRaceType(e.value);
   };
 
+  const getDataForTable = async () => {
+    const colRef = collection(db, "2023/Races/Bahrain");
+    const colSnap = await getDocs(colRef);
+    colSnap.forEach((doc) => {
+      databaseSessions.push(doc.id);
+      databaseData.push(doc.data());
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
+  getDataForTable();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <input
-            type="text"
-            id="raceNameInput"
-            name="raceNameInput"
-            onChange={handleRaceNameChange}
-            value={addRaceName}
-          />
-          <input type="button" onClick={handleRaceAdd} value="Add Race"></input>
-        </div>
-        <div>
+      <header>
+        <h1>Formula 1 Database</h1>
+      </header>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Position</th>
+              {databaseSessions.map((session) => {
+                return <th>{session}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {databaseData.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <input
+          type="button"
+          value="Update Table"
+          onClick={getDataForTable}
+        ></input>
+      </div>
+      <div>
+        <input
+          type="text"
+          id="raceNameInput"
+          name="raceNameInput"
+          onChange={handleRaceNameChange}
+          value={addRaceName}
+        />
+        <input type="button" onClick={handleRaceAdd} value="Add Race"></input>
+      </div>
+      {/*<div>
           <Select
             options={raceNamesSelectOptions}
             onChange={handleSelectRaceNameChange}
@@ -110,8 +125,7 @@ function App() {
             onClick={handleRaceUpdate}
             value="Update Race"
           ></input>
-        </div>
-      </header>
+        </div>*/}
     </div>
   );
 }
