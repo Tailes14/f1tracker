e1ce27cebf2d2d9f9964478613c39862f65e49f3
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2023-03-03 22:49:10 -0500

    creates placeholder files for each grand prix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import {
+  collection,
+  getDocs,
+  listCollections,
+  batch,
+  setDoc,
+  addDoc,
+  doc,
+} from "firebase/firestore";
+import "./App.css";
+import { db } from "./firebase";
+import { useState } from "react";
+
+const raceFiles = ["FP1", "FP2", "FP3", "Qualifying", "Race"];
+
+const placeholderRaceData = {
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
+  6: "",
+  7: "",
+  8: "",
+  9: "",
+  10: "",
+  11: "",
+  12: "",
+  13: "",
+  14: "",
+  15: "",
+  16: "",
+  17: "",
+  18: "",
+  19: "",
+  20: "",
+};
 
 function App() {
+  const [raceName, setRaceName] = useState("");
+
+  const addRace = async () => {
+    //const raceRef = collection(db, `2023/Races/${raceName}`);
+    for (const raceFile of raceFiles) {
+      await setDoc(
+        doc(db, `2023/Races/${raceName}/${raceFile}`),
+        placeholderRaceData
+      );
+    }
+    console.log("Docs created for " + raceName + "Grand Prix");
+  };
+  const getRaces = async () => {
+    //const raceRef = collection(db, "2023", "Races", "Bahrain");
+    /*
+    await getDocs(collection(db, "2023")).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        getDocs(collection(db, "2023", doc.id)).then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+          });
+        });
+      });
+    });
+    */
+  };
+
+  const handleChange = (e) => {
+    setRaceName(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +85,16 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <input
+            type="text"
+            id="raceName"
+            name="raceName"
+            onChange={handleChange}
+            value={raceName}
+          />
+          <input type="button" onClick={addRace}></input>
+        </div>
       </header>
     </div>
   );
